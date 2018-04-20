a26ec37ad336642e9e4b1c815088bf1a928dc5ab
Peter Kim <peterckim96@gmail.com>
2018-04-20 04:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign({}, object)
+  var newObj = Object.assign({}, object);
   newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
   return newObj;
 }
